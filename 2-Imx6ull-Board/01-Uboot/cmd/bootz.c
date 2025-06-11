1f82da966beda6472dcabbe41a5952c80a44e412
wohaishiayan <9448977+wohaishiayan@user.noreply.gitee.com>
2025-06-11 23:25:47 +0800

    [CHG]增加uboot解析设备树emmc分区脚本

--- 2-Imx6ull-Board/01-Uboot/cmd/bootz.c
+++ 2-Imx6ull-Board/01-Uboot/cmd/bootz.c
@@ -11,6 +11,9 @@
 #include <lmb.h>
 #include <log.h>
 #include <linux/compiler.h>
+#include <fdtdec.h>
+#include <fdt.h>
+
 
 int __weak bootz_setup(ulong image, ulong *start, ulong *end)
 {
@@ -70,6 +73,112 @@ static int bootz_start(struct cmd_tbl *cmdtp, int flag, int argc,
 	return 0;
 }
 
+void parse_partitions(const void *fdt, int node_offset) {
+   int subnode_offset;
+   int len;
+   const char *label;
+   const fdt32_t *reg;
+   int reg_len;
+
+   // 遍历 partitions 节点中的所有子节点
+   for (subnode_offset = fdt_first_subnode(fdt, node_offset);
+        subnode_offset >= 0;
+        subnode_offset = fdt_next_subnode(fdt, subnode_offset)) {
+       // 获取 label 属性
+       label = fdt_getprop(fdt, subnode_offset, "label", &len);
+       if (label) {
+           printf("Label: %s\n", label);
+       }
+
+       // 获取 reg 属性
+       reg = fdt_getprop(fdt, subnode_offset, "reg", &reg_len);
+       if (reg) {
+           uint32_t addr = fdt32_to_cpu(reg[0]); // 32-bit address
+           uint32_t size = fdt32_to_cpu(reg[1]); // 32-bit size
+           printf("  Address: 0x%x, Size: 0x%x\n", addr, size);
+       }
+   }
+}
+
+int find_usdhc2_node(const void *fdt, int node_offset) {
+   const char *compatible;
+   int len;
+
+  // 检查当前节点是否是 &usdhc2
+  compatible = fdt_getprop(fdt, node_offset, "obcpart", &len);
+  if (compatible && (strstr(compatible, "obc-emmc"))) {
+      return node_offset;
+  }
+
+   // 递归查找子节点
+   int subnode_offset;
+   for (subnode_offset = fdt_first_subnode(fdt, node_offset);
+        subnode_offset >= 0;
+        subnode_offset = fdt_next_subnode(fdt, subnode_offset)) {
+       int found_node = find_usdhc2_node(fdt, subnode_offset);
+       if (found_node >= 0) {
+           return found_node;
+       }
+   }
+
+   return -1;
+}
+
+void parse_device_tree(const void *fdt) {
+   int node_offset;
+   int partitions_node;
+
+   // 从根节点开始查找 &usdhc2 节点
+   node_offset = find_usdhc2_node(fdt, 0);
+   if (node_offset < 0) {
+       printf("Error: usdhc2 node not found\n");
+       return;
+   }
+
+   // 打印找到的节点信息
+   const char *node_name = fdt_get_name(fdt, node_offset, NULL);
+   if (node_name) {
+       printf("Found usdhc2 node: %s\n", node_name);
+   } else {
+       printf("Error: Unable to get name of usdhc2 node\n");
+       return;
+   }
+
+   // 查找 partitions 节点
+   partitions_node = fdt_subnode_offset(fdt, node_offset, "partitions");
+   if (partitions_node < 0) {
+       printf("Error: partitions node not found\n");
+
+       // 打印 &usdhc2 节点的所有子节点，以确认 partitions 节点是否存在
+       printf("Listing all subnodes of usdhc2 node:\n");
+       int subnode_offset;
+       for (subnode_offset = fdt_first_subnode(fdt, node_offset);
+            subnode_offset >= 0;
+            subnode_offset = fdt_next_subnode(fdt, subnode_offset)) {
+           const char *subnode_name = fdt_get_name(fdt, subnode_offset, NULL);
+           if (subnode_name) {
+               printf("  Subnode: %s\n", subnode_name);
+           } else {
+               printf("  Error: Unable to get name of subnode\n");
+           }
+       }
+       return;
+   }
+
+   // 打印 partitions 节点信息
+   const char *partitions_name = fdt_get_name(fdt, partitions_node, NULL);
+   if (partitions_name) {
+       printf("Found partitions node: %s\n", partitions_name);
+   } else {
+       printf("Error: Unable to get name of partitions node\n");
+       return;
+   }
+
+   // 解析 partitions 节点
+   parse_partitions(fdt, partitions_node);
+}
+
+
 int do_bootz(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 {
 	struct bootm_info bmi;
@@ -97,6 +206,18 @@ int do_bootz(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 	if (argc > 2)
 		bmi.conf_fdt = argv[2];
 	bmi.cmd_name = "bootz";
+
+    const void *fdt_addr = (void *)0x83000000; // 假设设备树位于 0x83000000
+
+    // 检查设备树是否有效
+    if (fdt_check_header(fdt_addr) != 0) {
+        printf("Error: Invalid device tree header\n");
+        return -1;
+    }
+
+    // 解析设备树
+    parse_device_tree(fdt_addr);
+
 
 	ret = bootz_run(&bmi);
 
