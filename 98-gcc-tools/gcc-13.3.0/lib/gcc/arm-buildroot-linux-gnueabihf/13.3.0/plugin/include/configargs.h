358c971a9c629bdf6d388bc3a0c1794d75bb2a6c
justxiaoyan <hushanyan1136@163.com>
2025-06-22 14:55:59 +0800

    [ADD]交叉编译工具链

---
+++ 98-gcc-tools/gcc-13.3.0/lib/gcc/arm-buildroot-linux-gnueabihf/13.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/home/wanguo/01-buildroot/output/host --sysconfdir=/home/wanguo/01-buildroot/output/host/etc --enable-static --target=arm-buildroot-linux-gnueabihf --with-sysroot=/home/wanguo/01-buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --enable-plugins --enable-lto --with-gmp=/home/wanguo/01-buildroot/output/host --with-mpc=/home/wanguo/01-buildroot/output/host --with-mpfr=/home/wanguo/01-buildroot/output/host --with-pkgversion='Buildroot -gb3f1f2fee5' --with-bugurl=https://gitlab.com/buildroot.org/buildroot/-/issues --without-zstd --disable-libquadmath --disable-libquadmath-support --enable-tls --enable-threads --without-isl --without-cloog --with-abi=aapcs-linux --with-cpu=cortex-a7 --with-fpu=neon-vfpv4 --with-float=hard --with-mode=arm --enable-languages=c --with-build-time-tools=/home/wanguo/01-buildroot/output/host/arm-buildroot-linux-gnueabihf/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "cortex-a7" }, { "float", "hard" }, { "mode", "arm" }, { "fpu", "neon-vfpv4" }, { "tls", "gnu" } };
